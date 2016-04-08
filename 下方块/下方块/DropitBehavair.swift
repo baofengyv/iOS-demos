60332d5ea007d86c6f6ed72ef97073d6d849bf33
bifangyu <baofengyv@gmail.com>
2016-04-08 10:57:58 +0800

    add comment

--- 下方块/下方块/DropitBehavair.swift
+++ 下方块/下方块/DropitBehavair.swift
@@ -8,26 +8,33 @@
 
 import UIKit
 
+/*
+ *  这个自定义的behavair由 重力 和 碰撞 两种组成
+ *    方法：给视图添加behavior后 放入view里
+ *    方法：移除behavior后 从view中移除
+ *    方法：添加碰撞障碍物
+ */
+
 class DropitBehavair: UIDynamicBehavior {
-    
+    //MARK: 设置及初始化
     // 重力场
     let gravity = UIGravityBehavior()
-    //碰撞效果
-    lazy var collider:UICollisionBehavior = {
+    
+    // 碰撞效果  使用闭包初始化
+    var collider:UICollisionBehavior = {
         let $ =  UICollisionBehavior()
-        // 将边界转成碰撞的边缘
+        // 将view的边界 转成 碰撞边界
         $.translatesReferenceBoundsIntoBoundary = true
-        
         return $
     }()
     
-    lazy var dropBehavior:UIDynamicItemBehavior = {
+    // Item的行为设置
+    var dropBehavior:UIDynamicItemBehavior = {
         let $ = UIDynamicItemBehavior()
-//        $.allowsRotation = false //不允许旋转
-        $.elasticity = 0.9
+        //        $.allowsRotation = false //不允许旋转
+        $.elasticity = 0.9 // 碰撞弹性
         return $
     }()
-    
     
     override init() {
         super.init()
@@ -35,24 +42,33 @@ class DropitBehavair: UIDynamicBehavior {
         addChildBehavior(collider)
         addChildBehavior(dropBehavior)
     }
-    
-    // 给视图添加behavior
-    func addDrop(drop: UIView){
+    //MARK:方法
+    // 给视图添加behavior后 放入view里
+    func addDropToView_with_custome_behavior(drop: UIView){
+        
+        //这个地方得先添加到view里 再增加行为
+        // 关联的Animator 里 关联的View
         dynamicAnimator?.referenceView?.addSubview(drop)
+        
+        // 添加到 重力 碰撞 和 item行为中
         gravity.addItem(drop)
         collider.addItem(drop)
         dropBehavior.addItem(drop)
     }
     
-    func removeDrop(drop:UIView) {
+    // 移除behavior后 从view中移除
+    func removeDropFromView(drop:UIView) {
         gravity.removeItem(drop)
         collider.removeItem(drop)
         dropBehavior.removeItem(drop)
         drop.removeFromSuperview()
     }
     
-    func addBarrier(path:UIBezierPath,named name: String){
+    // 添加障碍
+    func addBarrierUseBoundaryAsCollidBoundary(path:UIBezierPath,named name: String){
+        //移除原有的同名障碍
         collider.removeBoundaryWithIdentifier(name)
+        //用path作为障碍的边界
         collider.addBoundaryWithIdentifier(name, forPath: path)
     }
 }
--- 下方块/下方块/DropitViewController.swift
+++ 下方块/下方块/DropitViewController.swift
@@ -10,7 +10,9 @@ import UIKit
 
 class DropitViewController: UIViewController,UIDynamicAnimatorDelegate {
     
+    // 显示所有内容的View
     @IBOutlet weak var gameView: BeizePathView!
+    
     // 方框大小为宽度的十分之一
     var dropsPerRow = 10
     var dropSize:CGSize {
@@ -18,70 +20,71 @@ class DropitViewController: UIViewController,UIDynamicAnimatorDelegate {
         return CGSize(width: size, height: size)
     }
     
-
+    // 自定义的behavior（对重力和碰撞做了组合）
     let dropitBehavior = DropitBehavair()
-    var attachment:UIAttachmentBehavior? {
-        willSet{
-            if attachment != nil {
-                animator.removeBehavior(attachment!)
-            }
-            gameView.setPath(nil, named: PathNames.Attachment)
-        }
-        didSet{
-            if attachment != nil {
-                animator.addBehavior(attachment!)
-                attachment?.action = { [unowned self] in
-                    if let attachedView = self.attachment?.items.first as? UIView {
-                        let path = UIBezierPath()
-                        path.moveToPoint(self.attachment!.anchorPoint)
-                        path.addLineToPoint(attachedView.center)
-                        self.gameView.setPath(path, named: PathNames.Attachment)
-                    }
-                }
-            }
-        }
-    }
+    // 保存落下的最后一个方块
+    var lastDroppedView: UIView?
     
-    // 这样初始化的时候会有问题 
-    //   这行代码执行在初始化阶段 不能引用实例变量  等实例初始化完成后才能应用实例的属性
+    // 这样初始化的时候会有问题
+    //   这行代码执行在初始化阶段 不能引用实例的属性  等实例初始化完成后才能引用实例的属性
     //   var animator = UIDynamicAnimator(referenceView: gameView)
     
     // 一种方法是：在这里设成nil 在viewDidLoad 中执行初始化
     // 另一种是： 将属性设成lazy 并用闭包初始化它
+    
     lazy var animator:UIDynamicAnimator
-    /*这个类型必须制定，估计是：使用闭包进行初始化时，不能使用类型推断 要指定其类型*/
-    = {
-        let $ = UIDynamicAnimator(referenceView: self.gameView)
-        $.delegate = self
-        return $
+        /*这个类型必须制定，估计是：使用闭包进行初始化时，不能使用类型推断 要指定其类型*/
+        = {
+            // 应用view创建animator
+            let $ = UIDynamicAnimator(referenceView: self.gameView)
+            $.delegate = self
+            return $
     }()
-
+    // 当动画停下来时调用 检测哪行被填满了 然后将其删除
     func dynamicAnimatorDidPause(animator: UIDynamicAnimator) {
         removeCompleteRow()
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        // 给 animator 添加 behavior
         animator.addBehavior(dropitBehavior)
-
     }
+
     struct PathNames {
         static let MiddleBarrier = "Middle Barrier"
         static let Attachment = "Attachment"
     }
+    
+    // 只画一次圈 不用每次都画
+    var drawCircle_flag = false
+    // 当subview完成layout后调用
+    // 每次往里面添加view（添加方块）的时候都会调用
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        let barrierSize = dropSize
-        let barrierOrigin = CGPoint(x: gameView.bounds.midX - barrierSize.width/2, y: gameView.bounds.midY - barrierSize.height/2)
-        let path = UIBezierPath(ovalInRect: CGRect(origin: barrierOrigin, size:barrierSize))
-        
-        dropitBehavior.addBarrier(path, named: PathNames.MiddleBarrier)
-        gameView.setPath(path, named: PathNames.MiddleBarrier)
+        if !drawCircle_flag {
+            // 圆的直径
+            let barrierSize = dropSize
+            // 左上角起点坐标
+            let barrierOrigin = CGPoint(x: gameView.bounds.midX - barrierSize.width/2, y: gameView.bounds.midY - barrierSize.height/2)
+            // 矩形里面画一个圆的path
+            let path = UIBezierPath(ovalInRect: CGRect(origin: barrierOrigin, size:barrierSize))
+            
+            // 将path作为碰撞的边界添加套behavior
+            dropitBehavior.addBarrierUseBoundaryAsCollidBoundary(path, named: PathNames.MiddleBarrier)
+            
+            // 将path显示出来
+            gameView.setPath(path, named: PathNames.MiddleBarrier)
+            
+            drawCircle_flag = true
+        }
     }
+    
+    // tap手势 点击手势
     @IBAction func taping(sender: UITapGestureRecognizer) {
         taping()
     }
+    // tap时 添加一个方块
     func taping(){
         //创建一个frame 用这个frame创建UIView 将UIView添加到界面中
         //   创建一个frame x坐标是随机的
@@ -94,40 +97,26 @@ class DropitViewController: UIViewController,UIDynamicAnimatorDelegate {
         
         lastDroppedView = rectView
         
-        dropitBehavior.addDrop(rectView)
+        // 将view添加到行为里面
+        dropitBehavior.addDropToView_with_custome_behavior(rectView)
     }
     
-    @IBAction func grabDrop(sender: UIPanGestureRecognizer) {
-        
-        // 手势在哪里
-        let gesturePoint = sender.locationInView(gameView)
-        switch sender.state {
-        case .Began:
-
-            if let $ = lastDroppedView {
-                attachment = UIAttachmentBehavior(item: $,attachedToAnchor: gesturePoint)
-                lastDroppedView = nil
-            }
-        case .Changed:
-            attachment?.anchorPoint = gesturePoint
-        case .Ended:
-            attachment = nil
-        default:
-            break
-        }
-        
-    }
-    var lastDroppedView: UIView?
-    
+    // 删除填满一行的方块
     func removeCompleteRow(){
+        
         var dropsToRemove = [UIView]()
         var dropFrame = CGRect(x:0,y:gameView.frame.maxY,width: dropSize.width,height: dropSize.height)
+        
+        // 从最下面开始 一行行向上扫描
         repeat {
+            
             dropFrame.origin.y -= dropSize.height
             dropFrame.origin.x = 0
             var dropsFound = [UIView]()
             var rowIsComplete = true
+            
             for _ in 0 ..< dropsPerRow {
+                // 命中检测
                 if let hitView = gameView.hitTest(CGPoint(x:dropFrame.midX,y:dropFrame.midY), withEvent: nil){
                     if hitView.superview == gameView {
                         dropsFound.append(hitView)
@@ -137,23 +126,78 @@ class DropitViewController: UIViewController,UIDynamicAnimatorDelegate {
                 }
                 dropFrame.origin.x += dropSize.width
             }
+            
             if rowIsComplete {
                 dropsToRemove += dropsFound
             }
+            
         } while dropsToRemove.count == 0 && dropFrame.origin.y > 0
     
+        // 移除需要移除的
         for drop in dropsToRemove {
-            dropitBehavior.removeDrop(drop)
+            dropitBehavior.removeDropFromView(drop)
+        }
+    }
+    
+    // 将方块和活动手势的锚点绑定
+    var attachment:UIAttachmentBehavior? {
+        willSet{
+            //删除原来的attachment 移除原来的path
+            if attachment != nil {
+                animator.removeBehavior(attachment!)
+            }
+            gameView.setPath(nil, named: PathNames.Attachment)
+        }
+        didSet{
+            // 添加 attachment 到 animator
+            if attachment != nil {
+                animator.addBehavior(attachment!)
+                
+                // 动画执行的时候每次调用
+                attachment?.action = { [unowned self] in
+                    if let attachedView = self.attachment?.items.first as? UIView {
+                        // 画一条从手指到方块中心的线
+                        let path = UIBezierPath()
+                        path.moveToPoint(self.attachment!.anchorPoint)
+                        path.addLineToPoint(attachedView.center)
+                        self.gameView.setPath(path, named: PathNames.Attachment)
+                    }
+                }
+            }
+        }
+    }
+    //拖动手势
+    @IBAction func grabDrop(sender: UIPanGestureRecognizer) {
+        // 手指在哪里
+        let gesturePoint = sender.locationInView(gameView)
+        
+        switch sender.state {
+        case .Began: //手势开始
+            if let $ = lastDroppedView {
+                // 将手势的那个点 和 lastDroppedView 建立attachment
+                attachment = UIAttachmentBehavior(item: $,attachedToAnchor: gesturePoint)
+                lastDroppedView = nil
+            }
+        case .Changed: //手势拖动
+            // 改变锚点到手指的新位置
+            attachment?.anchorPoint = gesturePoint
+        case .Ended:  //手势结束
+            attachment = nil
+        default:
+            break
         }
     }
 }
 
+//MARK: 扩展
+
+// 返回 0～max之间的随机数
 private extension CGFloat {
     static func random(max:Int) -> CGFloat {
         return CGFloat(arc4random() % UInt32(max))
     }
 }
-
+// 返回一个随机颜色
 private extension UIColor {
     // class var 跟 static var相似
     // 用class修饰的可以被子类改变
