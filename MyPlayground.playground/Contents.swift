4249eceb27b6ca13b1e8a62dfde9315498f54d96
bifangyu <baofengyv@gmail.com>
2016-04-12 09:12:14 +0800

    remove scrach

--- MyPlayground.playground/Contents.swift
+++
@@ -1,7 +0,0 @@
-//: Playground - noun: a place where people can play
-
-import UIKit
-
-
-
-let timer = NSTimer.scheduledTimerWithTimeInterval(2, target: self, selector: "fire:", userInfo: nil, repeats: true)
